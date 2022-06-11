2fcf7cf86230daf425bc6e031e182974dcb50bba
SaratAngajalaoffl <saratangajala@gmail.com>
2022-06-11 16:29:39 +0530

    Animation Bug fix for Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,60 +12,69 @@ const Navbar = () => {
   const [sidebar, setsidebar] = useState(false);
   const { pathname } = useLocation();
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{
-        opacity: [0, 1],
-      }}
-      transition={{
-        delay: sidebar ? 0 : 9,
-        duration: sidebar ? 0 : 3,
-        ease: "backInOut",
-      }}
-      className={classes.Container}>
-      <Link to='/home' className={classes.logo}>
-        <img src={Logo} alt='Logo' />
-      </Link>
-      <div className={classes.Links}>
-        <Link to='/home'>
-          <div
-            className={
-              pathname === "/home" ? classes.active : classes.inactive
-            }>
-            Home
-          </div>
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: [0, 1],
+        }}
+        transition={{
+          delay: sidebar ? 0 : 9,
+          duration: sidebar ? 0 : 3,
+          ease: "backInOut",
+        }}
+        className={classes.Container}
+      >
+        <Link to="/home" className={classes.logo}>
+          <img src={Logo} alt="Logo" />
         </Link>
-        <Link to='/about'>
-          <div
-            className={
-              pathname === "/about" ? classes.active : classes.inactive
-            }>
-            About
-          </div>
-        </Link>
-        <Link to='/documents'>
-          <div
-            className={
-              pathname === "/documents" ? classes.active : classes.inactive
-            }>
-            Documents
-          </div>
-        </Link>
-        <Link to='/faq'>
-          <div
-            className={pathname === "/faq" ? classes.active : classes.inactive}>
-            FAQ
-          </div>
-        </Link>
-      </div>
-      <div className={classes.mobileNavbar}>
-        <div onClick={() => setsidebar(true)} className={classes.BurgerIcon}>
-          <img src={Hamburger} alt='Hamburger' />
+        <div className={classes.Links}>
+          <Link to="/home">
+            <div
+              className={
+                pathname === "/home" ? classes.active : classes.inactive
+              }
+            >
+              Home
+            </div>
+          </Link>
+          <Link to="/about">
+            <div
+              className={
+                pathname === "/about" ? classes.active : classes.inactive
+              }
+            >
+              About
+            </div>
+          </Link>
+          <Link to="/documents">
+            <div
+              className={
+                pathname === "/documents" ? classes.active : classes.inactive
+              }
+            >
+              Documents
+            </div>
+          </Link>
+          <Link to="/faq">
+            <div
+              className={
+                pathname === "/faq" ? classes.active : classes.inactive
+              }
+            >
+              FAQ
+            </div>
+          </Link>
         </div>
-        <button className={classes.signUpBtn}>Sign Up</button>
-      </div>
+        <div className={classes.mobileNavbar}>
+          <div onClick={() => setsidebar(true)} className={classes.BurgerIcon}>
+            <img src={Hamburger} alt="Hamburger" />
+          </div>
+          <button className={classes.signUpBtn}>Sign Up</button>
+        </div>
+      </motion.div>
       <Sidebar sideBar={sidebar} setSideBar={setsidebar} />
-    </motion.div>
+    </>
   );
 };
 
